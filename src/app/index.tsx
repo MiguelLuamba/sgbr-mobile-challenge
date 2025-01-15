e280bbf6012bd685b1333e524b35e89753bc3b27
miguelluamba <migueltchaquarta@gmail.com>
2025-01-15 23:55:16 +0100

    modify project setup

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import "@/styles/global.css";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-red-700"
+    >
+      <Text className="text-2xl font-bold text-zinc-200">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
